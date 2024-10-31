2e0f8f6cdc36f667c1ca5551202ffbeccf55b76a
Sengleang99 <yuleang18@gmail.com>
2024-10-31 11:59:05 +0700

    update intitail about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,10 +17,7 @@ function About() {
   });
 
   return (
-    <section
-      className="py-20 text-white bg-gradient-to-r from-blue-700 to-purple-700"
-      id="about"
-    >
+    <section className="py-20 text-gray-800 bg-white" id="about">
       <div className="container flex flex-col items-center justify-center h-full px-8 mx-auto md:px-16 lg:px-24">
         <h2
           className={`mb-12 text-4xl font-extrabold tracking-tight text-center ${
@@ -34,7 +31,7 @@ function About() {
         <div className="flex flex-col items-center md:items-center">
           <div className="w-full md:w-2/3">
             <p
-              className={`mb-8 text-xl font-medium text-center text-white lg:mb-16 sm:text-2xl ${
+              className={`mb-8 text-xl text-center text-gray-700 lg:mb-16 sm:text-xl ${
                 isParagraphInView ? "opacity-100" : "opacity-0"
               } ${isParagraphInView ? "translate-y-0" : "translate-y-5"}`}
               ref={paragraphRef}
@@ -49,7 +46,7 @@ function About() {
             </p>
             <div className="space-y-8">
               <h2
-                className={`text-4xl font-bold shadow-md ${
+                className={`text-4xl font-bold text-gray-800 shadow-md ${
                   isSkillsInView ? "opacity-100" : "opacity-0"
                 } ${isSkillsInView ? "translate-y-0" : "translate-y-5"}`}
                 ref={skillsRef}
@@ -65,77 +62,38 @@ function About() {
               <SkillProgress skill="Flutter" level="55%" />
               <SkillProgress skill="Firebase" level="55%" />
               <SkillProgress skill="Figma" level="65%" />
-              {/* Add more skills as needed */}
             </div>
             <div className="mt-12">
               {/* Education Section */}
-              <h1
-                className="text-2xl font-extrabold shadow-md sm:text-3xl md:text-4xl"
-                data-aos="fade-up"
-                data-aos-delay="600"
-              >
+              <h1 className="text-2xl font-extrabold text-gray-800 shadow-md sm:text-3xl md:text-4xl">
                 Education
               </h1>
-              <div
-                className="p-4 mt-6 rounded-lg shadow-lg sm:p-6 md:p-8 md:mt-8 bg-gradient-to-r from-purple-600 to-blue-600"
-                data-aos="fade-up"
-              >
-                <div
-                  className="flex flex-col items-start space-y-4 sm:flex-row sm:space-y-0 sm:space-x-6"
-                  data-aos="fade-up"
-                >
-                  <div className="text-2xl">
-                    <FaGraduationCap />
-                  </div>
-                  <div className="flex flex-col">
-                    <h3 className="text-lg font-bold sm:text-xl md:text-2xl">
-                      S.E. in Software Engineering
-                    </h3>
-                    <p className="mt-2 text-base font-medium text-white sm:text-lg">
-                      <span className="block">
-                        Beltei International University, currently in progress
-                      </span>
-                      <span className="block mt-4">Web Development</span>
-                      <span className="block mt-4">Data Structures</span>
-                      <span className="block mt-4">Database Management</span>
-                    </p>
-                  </div>
-                </div>
+              <div className="mt-6 space-y-6">
+                <ResumeCard
+                  title="High School Degree"
+                  institution="Hunsen Srah Bonteay High School"
+                  year="2018 - 2019"
+                  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                />
+                <ResumeCard
+                  title="Bachelor's Degree in Software Engineering"
+                  institution="Beltei International University"
+                  year="2024 - Third year in Progress"
+                  description="Specialized in Web Development, Data Structures, and Database Management."
+                />
               </div>
 
               {/* Experience Section */}
-              <h1
-                className="mt-12 text-2xl font-extrabold shadow-md sm:text-3xl md:text-4xl"
-                data-aos="fade-up"
-              >
+              <h1 className="mt-12 text-2xl font-extrabold text-gray-800 shadow-md sm:text-3xl md:text-4xl">
                 Experience
               </h1>
-              <div
-                className="p-4 mt-6 rounded-lg shadow-lg sm:p-6 md:p-8 md:mt-8 bg-gradient-to-r from-purple-600 to-blue-600"
-                data-aos="fade-up"
-              >
-                <div className="flex flex-col items-start space-y-4 sm:flex-row sm:space-y-0 sm:space-x-6">
-                  <div className="text-2xl">
-                    <FaLaptopCode />
-                  </div>
-                  <div className="flex flex-col">
-                    <h3 className="text-lg font-bold sm:text-xl md:text-2xl">
-                      P-Hearth Project (Volunteer 3 months)
-                    </h3>
-                    <p className="mt-2 text-base font-medium text-white sm:text-lg">
-                      <span className="block mt-4">
-                        I work as software developer
-                      </span>
-                      <span className="block mt-4">
-                        Create mobile app using flutter and firebase
-                      </span>
-                      <span className="block mt-4">
-                        Create prototype using figma
-                      </span>
-                      <span className="block mt-4">Analytics database</span>
-                    </p>
-                  </div>
-                </div>
+              <div className="mt-6 space-y-6">
+                <ResumeCard
+                  title="Software Developer"
+                  institution="Poscar Digital Co,.LTD."
+                  year="Valunteer 3 months - 2024"
+                  description="Build project full stack mobile appliction with team. Use tool flutter firebase figma(UX UI). Work hard researching document and analyze table database."
+                />
               </div>
             </div>
           </div>
@@ -161,13 +119,26 @@ function SkillProgress({ skill, level }) {
         transition: "opacity 0.5s ease-out, transform 0.5s ease-out",
       }}
     >
-      <span className="w-3/12 text-left">{skill}</span>
-      <div className="relative w-9/12 h-2.5 bg-slate-100 rounded-full overflow-hidden shadow-md">
+      <span className="w-3/12 text-left text-gray-700">{skill}</span>
+      <div className="relative w-9/12 h-2.5 bg-gray-300 rounded-full overflow-hidden shadow-md">
         <div
-          className="absolute top-0 left-0 h-full bg-gradient-to-r from-pink-400 to-blue-500 hover:scale-105"
+          className="absolute top-0 left-0 h-full bg-gradient-to-r from-blue-500 to-indigo-500"
           style={{ width: level, transition: "width 0.5s ease-out" }}
         ></div>
       </div>
+    </div>
+  );
+}
+
+function ResumeCard({ title, institution, year, description }) {
+  return (
+    <div className="p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+      <div className="flex items-center justify-between">
+        <h3 className="text-xl font-semibold text-gray-800">{title}</h3>
+        <span className="text-sm font-medium text-gray-600">{year}</span>
+      </div>
+      <p className="mt-2 text-gray-600">{institution}</p>
+      <p className="mt-4 text-gray-700">{description}</p>
     </div>
   );
 }
